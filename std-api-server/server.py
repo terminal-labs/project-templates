1210bf2c0a7fed181e89384664f02dce1196d18d
Michael Verhulst <michael@terminallabs.com>
2018-02-20 23:22:21 -0600

    first commit

---
+++ std-api-server/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+version = "0.2"
+api_spec_version = "0.1"
+status = 'good'
+
+test = {
+        'status': status,
+        }
+
+info = {
+        'status': status,
+        'version': version,
+        'api-spec-version': api_spec_version,
+        }
+
+@app.route('/api/v1.0/system/test', methods=['GET'])
+def system_test():
+    return jsonify(test)
+
+@app.route('/api/v1.0/system/info', methods=['GET'])
+def system_general_info():
+    return jsonify(info)
+
+if __name__ == '__main__':
+    app.run(debug=True)
