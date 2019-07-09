29c0e02288a887a3a20cf0ba93fac2c2c15f28af
verhulstm <michael@terminallabs.com>
2019-07-08 22:37:31 -0500

    removed deprecated/pex-cli-tool as it is now in python-environment-manager repo

--- deprecated/pex-cli-tool/build.sh
+++
@@ -1,6 +0,0 @@
-rm app.pex
-pip wheel -w . .
-pex --python=python3 -f $PWD click cli-passthrough cli -e app.main -o app.pex
-rm cli-0.0.0-py3-none-any.whl
-rm -rf ~/.pex
-./app.pex
--- deprecated/pex-cli-tool/setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup
-setup (
-  name = "cli",
-  packages=['app']
-)
