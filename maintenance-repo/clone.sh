6e77a65cc5a00fba44ed5b9a3be673ae54726d3f
Michael Verhulst <michael@terminallabs.com>
2024-03-06 02:34:49 -0600

    Delete maintenance-repo directory

--- maintenance-repo/clone.sh
+++
@@ -1,3 +0,0 @@
-git clone git@github.com:terminal-labs/repo-1.git --recursive ../repo-1
-git clone git@github.com:terminal-labs/repo-2.git --recursive ../repo-2
-git clone git@github.com:terminal-labs/repo-3.git --recursive ../repo-3
--- maintenance-repo/install_deps.sh
+++
@@ -1,3 +0,0 @@
-pip install -r ../repo-1/requirements.txt
-pip install -r ../repo-2/requirements.txt 
-pip install -r ../repo-3/requirements.txt
